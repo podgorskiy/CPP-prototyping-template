cc6b4c90ea0e24df5a38202c8a5cc29b63e79c7f
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2020-09-12 23:50:48 -0400

    Tests

--- sources/chessis/tests.cpp
+++ sources/chessis/tests.cpp
@@ -7,16 +7,19 @@ TEST_CASE("[BASIC] moves")
 	SUBCASE("Basic")
 	{
 		chessis::Board board;
-		chessis::make_board(board, ".pp##...\n"
-		                           "....#...\n"
-		                           "..#.....\n"
-		                           "......##\n"
-		                           "##......\n"
-		                           ".....#..\n"
-		                           "...#....\n"
-		                           "...##PP.\n");
+		chessis::make_board(board, "####..\n"
+		                           "#a....\n"
+		                           "...A..\n"
+		                           "......\n"
+		                           "......\n");
 
-		auto move1 = chessis::FindBestMove(board, 12, chessis::Turn::BlackPlay);
+		auto move1 = chessis::FindBestMove(board, 6, chessis::Turn::WhitePLay);
+		CHECK_EQ(move1.action, chessis::Move::move);
+		CHECK_EQ(move1.dir, chessis::Direction::Left);
+		CHECK_EQ(move1.op_id, 0);
+		CHECK_EQ(move1.new_eval, 1000 * 100 + board.white_total_health + 3);
+
+		printf("a");
 
 //		REQUIRE_THROWS({
 //			Render::Uniform aa(-1, Render::VarType::BYTE, 1);
@@ -26,3 +29,8 @@ TEST_CASE("[BASIC] moves")
 
 	}
 }
+
+
+
+
+
